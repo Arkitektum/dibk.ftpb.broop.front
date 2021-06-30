151177b2212256ee968cdaa49ecefe44024ee08e
Benjamin Dehli <superelg@gmail.com>
2021-06-30 15:38:43 +0200

    Add support for environment config file

---
+++ config-overrides.js
@@ -0,0 +1,11 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = function override(config, env) {
+    const configPath = process.env.NODE_ENV === 'development' ? 'src/config/config.local.json' : 'src/config/config.json';
+    config.plugins.push(new CopyWebpackPlugin({
+        patterns: [
+            { from: configPath, to: 'config.json', toType: 'file' }
+        ]
+    }));
+    return config;
+};
