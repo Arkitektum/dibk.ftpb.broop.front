2a656e3845e248fdca5dd5d3a94e112e48eb3e44
Benjamin Dehli <superelg@gmail.com>
2021-07-02 16:34:44 +0200

    Remove unused boilerplate content

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,13 +3,11 @@ import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 
 // Reducers
-import NameReducer from 'reducers/NameReducer';
 import SelectedFormReducer from 'reducers/SelectedFormReducer';
 import SelectedSubmissionReducer from 'reducers/SelectedSubmissionReducer';
 
 const reducers = history => combineReducers({
   router: connectRouter(history),
-  name: NameReducer,
   selectedForm: SelectedFormReducer,
   selectedSubmission: SelectedSubmissionReducer
 });
