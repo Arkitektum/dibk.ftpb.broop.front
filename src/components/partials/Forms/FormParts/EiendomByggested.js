21017801b4158183db06435492bffd37f81f3561
Benjamin Dehli <superelg@gmail.com>
2021-07-08 15:55:21 +0200

    Remove unused render method

--- src/components/partials/Forms/FormParts/EiendomByggested.js
+++ src/components/partials/Forms/FormParts/EiendomByggested.js
@@ -10,43 +10,6 @@ import { formatAddress } from 'helpers/formatHelpers';
 import formsStyle from 'components/partials/Forms/Forms.module.scss';
 
 class EiendomByggested extends Component {
-    renderEiendomByggestedList(eiendomByggestedList) {
-        return eiendomByggestedList.map((eiendomByggestedListItem, index) => {
-            return (
-                <div key={index}>
-                    <dl className={formsStyle.fieldList}>
-                        <div className={formsStyle.flex25}>
-                            <dt>Gårdsnr.</dt><dd>{eiendomByggestedListItem.eiendomsidentifikasjon?.gaardsnummer}</dd>
-                        </div>
-                        <div className={formsStyle.flex25}>
-                            <dt>Bruksnr.</dt><dd>{eiendomByggestedListItem.eiendomsidentifikasjon?.bruksnummer}</dd>
-                        </div>
-                        <div className={formsStyle.flex25}>
-                            <dt>Festenr.</dt><dd>{eiendomByggestedListItem.eiendomsidentifikasjon?.festenummer}</dd>
-                        </div>
-                        <div className={formsStyle.flex25}>
-                            <dt>Seksjonsnr.</dt><dd>{eiendomByggestedListItem.eiendomsidentifikasjon?.seksjonsnummer}</dd>
-                        </div>
-
-                        <div className={formsStyle.flex50}>
-                            <dt>Bygningsnr.</dt><dd>{eiendomByggestedListItem.bygningsnummer}</dd>
-                        </div>
-                        <div className={formsStyle.flex50}>
-                            <dt>Bolignr.</dt><dd>{eiendomByggestedListItem.bolignummer}</dd>
-                        </div>
-
-                        <div className={formsStyle.flex100}>
-                            <dt>Kommunenummer.</dt><dd>{eiendomByggestedListItem.eiendomsidentifikasjon?.kommunenummer}</dd>
-                        </div>
-                        <div className={formsStyle.flex100}>
-                            <dt>Adresse</dt><dd>{formatAddress(eiendomByggestedListItem.adresse)}</dd>
-                        </div>
-                    </dl>
-                </div>
-            )
-        })
-    }
-
     render() {
         return this.props.eiendomByggested?.length
             ? this.props.eiendomByggested.map((eiendomByggestedListItem, index) => {
