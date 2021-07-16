b9c5d0f1e8ff6586af3fe22f4f245a51eaf3285a
Benjamin Dehli <superelg@gmail.com>
2021-07-16 14:11:13 +0200

    Add component for first 'samsvarserklæring' step

---
+++ src/components/partials/Forms/SamsvarsErklaering/Start.js
@@ -0,0 +1,66 @@
+// Dependencies
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+// DIBK Design
+import { Button, Header, InputField } from 'dibk-design';
+
+// Stylesheets
+import formsStyle from 'components/partials/Forms/Forms.module.scss';
+
+class KontrollErklaeringer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            prosjektnavn: 'Bakkebygrenda'
+        }
+    }
+
+    render() {
+        const formData = this.props.selectedForm?.formData;
+        return formData
+            ? (
+                <React.Fragment>
+                    <div className={formsStyle.headerSection}>
+                        <Header content="Samsvarserklæring"></Header>
+                        <span className={formsStyle.subtitle}>etter plan -og bygningsloven(pbl) § 23-1, SAK10 kapittel 12</span>
+                        <p>Etter signering blir erklæringen sendt til ansvarlig søker.</p>
+                    </div>
+                    <dl className={`${formsStyle.fieldList} ${formsStyle.inlineFieldList}`}>
+                        <div className={formsStyle.flex50}>
+                            <dt>Kommunens saksnummer (år/sekvensnummer):</dt>
+                            <dd>{formData.kommunensSaksnummer?.saksaar}/{formData.kommunensSaksnummer?.sakssekvensnummer}</dd>
+                        </div>
+                    </dl>
+                    <div className={formsStyle.inputGroup}>
+                        <div className={formsStyle.flex50}>
+                            <InputField
+                                id={`prosjektnavn`}
+                                onChange={(event) => { this.setState({ prosjektnavn: event.target.value }) }}
+                                label="Prosjektnavn"
+                                value={this.state.prosjektnavn} />
+                        </div>
+                    </div>
+                    <div className={`${formsStyle.buttonRow} ${formsStyle.reverse}`}>
+                        <div></div>
+                        <Link to={{ pathname: 'erklaeringenGjelder', search: window.location.search }}>
+                            <Button color="primary" content="Neste" arrow='right' />
+                        </Link>
+                    </div>
+                </React.Fragment>
+            )
+            : (
+                <p>Ingen data for samsvarserklæring</p>
+            )
+    }
+}
+
+const mapStateToProps = state => ({
+    selectedForm: state.selectedForm
+});
+
+
+export default connect(mapStateToProps, null)(KontrollErklaeringer);
+
+
