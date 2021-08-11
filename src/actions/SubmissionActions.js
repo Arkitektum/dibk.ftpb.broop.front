61bdaa219fdfb8bcf0d9f375cbda5dbff13dea9e
Benjamin Dehli <superelg@gmail.com>
2021-08-11 10:12:31 +0200

    Adjust fetch url for submission

--- src/actions/SubmissionActions.js
+++ src/actions/SubmissionActions.js
@@ -1,7 +1,8 @@
 import { FETCH_SUBMISSION} from 'constants/types';
 
 export const fetchSubmission = (guid) => dispatch => {
-    const apiUrl = `https://dibk-ftpb-broop-api.azurewebsites.net/api/v1/Innsending?referanseId=${guid}`
+    const apiUrl = `https://dibk-ftpb-broop-api.azurewebsites.net/api/v1/Innsending/${guid}`
+
     return fetch(apiUrl).then(res => res.json()).then(form => {
         return dispatch({ type: FETCH_SUBMISSION, payload: form })
     });
