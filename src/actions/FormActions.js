dff45c301e9d17ca9d3f4506d03dbbe1586db012
Benjamin Dehli <superelg@gmail.com>
2021-08-13 13:11:44 +0200

    Add action and reducer for 'fetch selected form'

--- src/actions/FormActions.js
+++ src/actions/FormActions.js
@@ -1,4 +1,17 @@
-import { UPDATE_SELECTED_FORM } from 'constants/types';
+// Types
+import { FETCH_SELECTED_FORM, UPDATE_SELECTED_FORM } from 'constants/types';
+
+// Helpers
+import { getEnvironmentVariable } from 'helpers/environmentVariableHelpers.js';
+
+export const fetchSelectedForm = (submission) => dispatch => {
+    const internalApiUrl = getEnvironmentVariable('internalApiUrl');
+    const formType = submission?.innsendingsType?.toLowerCase();
+    const formPath = submission._links?.[formType]?.href;
+    return fetch(`${internalApiUrl}${formPath}`).then(res => res.json()).then(form => {
+        return dispatch({ type: FETCH_SELECTED_FORM, payload: form })
+    });
+}
 
 export const updateSelectedForm = (form) => dispatch => {
     return dispatch({ type: UPDATE_SELECTED_FORM, payload: form })
--- src/reducers/SelectedFormReducer.js
+++ src/reducers/SelectedFormReducer.js
@@ -1,9 +1,11 @@
-import { UPDATE_SELECTED_FORM } from 'constants/types';
+import { FETCH_SELECTED_FORM, UPDATE_SELECTED_FORM } from 'constants/types';
 
 const initialState = {};
 
 const reducer = (state = initialState, action) => {
 	switch (action.type) {
+		case FETCH_SELECTED_FORM:
+			return action.payload;
 		case UPDATE_SELECTED_FORM:
 			return action.payload;
 		default:
