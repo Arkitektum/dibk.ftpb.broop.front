c7917b03321e5d4d4083c1aab9d0b589c389ea98
Benjamin Dehli <superelg@gmail.com>
2021-08-17 15:54:28 +0200

    Add logic and template for multiple eiendom/byggested items

--- src/components/partials/Forms/FormParts/EiendomByggested.js
+++ src/components/partials/Forms/FormParts/EiendomByggested.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
+// DIBK Design
+import { Accordion } from 'dibk-design';
+
 // Helpers
 import { formatAddress } from 'helpers/formatHelpers';
 
@@ -10,41 +13,62 @@ import { formatAddress } from 'helpers/formatHelpers';
 import formsStyle from 'components/partials/Forms/Forms.module.scss';
 
 class EiendomByggested extends Component {
+
+    renderEiendomByggestedItem(eiendomByggestedItem) {
+        return (
+            <dl key={0} className={formsStyle.fieldList}>
+                <div className={formsStyle.flex25}>
+                    <dt>Gårdsnr.</dt><dd>{eiendomByggestedItem.eiendomsidentifikasjon?.gaardsnummer}</dd>
+                </div>
+                <div className={formsStyle.flex25}>
+                    <dt>Bruksnr.</dt><dd>{eiendomByggestedItem.eiendomsidentifikasjon?.bruksnummer}</dd>
+                </div>
+                <div className={formsStyle.flex25}>
+                    <dt>Festenr.</dt><dd>{eiendomByggestedItem.eiendomsidentifikasjon?.festenummer}</dd>
+                </div>
+                <div className={formsStyle.flex25}>
+                    <dt>Seksjonsnr.</dt><dd>{eiendomByggestedItem.eiendomsidentifikasjon?.seksjonsnummer}</dd>
+                </div>
+
+                <div className={formsStyle.flex50}>
+                    <dt>Bygningsnr.</dt><dd>{eiendomByggestedItem.bygningsnummer}</dd>
+                </div>
+                <div className={formsStyle.flex50}>
+                    <dt>Bolignr.</dt><dd>{eiendomByggestedItem.bolignummer}</dd>
+                </div>
+
+                <div className={formsStyle.flex100}>
+                    <dt>Kommunenummer.</dt><dd>{eiendomByggestedItem.eiendomsidentifikasjon?.kommunenummer}</dd>
+                </div>
+                <div className={formsStyle.flex100}>
+                    <dt>Adresse</dt><dd>{formatAddress(eiendomByggestedItem.adresse)}</dd>
+                </div>
+            </dl>
+        )
+    }
+
+    renderEiendomByggestedListItem(eiendomByggestedItem, index) {
+        const kommunenummer = eiendomByggestedItem.eiendomsidentifikasjon?.kommunenummer;
+        const gaardsnummer = eiendomByggestedItem.eiendomsidentifikasjon?.gaardsnummer;
+        const bruksnummer = eiendomByggestedItem.eiendomsidentifikasjon?.bruksnummer;
+        const title = `${kommunenummer}/${gaardsnummer}/${bruksnummer}`;
+        return (
+            <div key={index} className={formsStyle.accordionItem}>
+                <Accordion title={title}>
+                    {this.renderEiendomByggestedItem(eiendomByggestedItem)}
+                </Accordion>
+            </div>
+        )
+    }
+
+    renderEiendomByggested() { }
+
     render() {
         return this.props.eiendomByggested?.length
-            ? this.props.eiendomByggested.map((eiendomByggestedListItem, index) => {
-                return (
-                    <div key={index}>
-                        <dl className={formsStyle.fieldList}>
-                            <div className={formsStyle.flex25}>
-                                <dt>Gårdsnr.</dt><dd>{eiendomByggestedListItem.eiendomsidentifikasjon?.gaardsnummer}</dd>
-                            </div>
-                            <div className={formsStyle.flex25}>
-                                <dt>Bruksnr.</dt><dd>{eiendomByggestedListItem.eiendomsidentifikasjon?.bruksnummer}</dd>
-                            </div>
-                            <div className={formsStyle.flex25}>
-                                <dt>Festenr.</dt><dd>{eiendomByggestedListItem.eiendomsidentifikasjon?.festenummer}</dd>
-                            </div>
-                            <div className={formsStyle.flex25}>
-                                <dt>Seksjonsnr.</dt><dd>{eiendomByggestedListItem.eiendomsidentifikasjon?.seksjonsnummer}</dd>
-                            </div>
-
-                            <div className={formsStyle.flex50}>
-                                <dt>Bygningsnr.</dt><dd>{eiendomByggestedListItem.bygningsnummer}</dd>
-                            </div>
-                            <div className={formsStyle.flex50}>
-                                <dt>Bolignr.</dt><dd>{eiendomByggestedListItem.bolignummer}</dd>
-                            </div>
-
-                            <div className={formsStyle.flex100}>
-                                <dt>Kommunenummer.</dt><dd>{eiendomByggestedListItem.eiendomsidentifikasjon?.kommunenummer}</dd>
-                            </div>
-                            <div className={formsStyle.flex100}>
-                                <dt>Adresse</dt><dd>{formatAddress(eiendomByggestedListItem.adresse)}</dd>
-                            </div>
-                        </dl>
-                    </div>
-                )
+            ? this.props.eiendomByggested.map((eiendomByggestedItem, index) => {
+                return this.props.eiendomByggested.length === 1
+                    ? this.renderEiendomByggestedItem(eiendomByggestedItem)
+                    : this.renderEiendomByggestedListItem(eiendomByggestedItem, index);
             })
             : (
                 <p>Ingen data for Eiendom/Byggested</p>
