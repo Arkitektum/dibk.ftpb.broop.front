7138710f8d7311d2c5afa9bb81a60de7e54e4516
Benjamin Dehli <superelg@gmail.com>
2021-08-23 13:02:59 +0200

    Add action for pdf-generator request

---
+++ src/actions/PrintActions.js
@@ -0,0 +1,26 @@
+import { saveAs } from 'file-saver';
+
+// Helpers
+import { getEnvironmentVariable } from 'helpers/environmentVariableHelpers.js';
+
+const saveFileContent = (blob) => {
+    const filename = "AnSaKo.pdf";
+    saveAs(blob, filename);
+}
+
+export const convertSelectedFormToPDF = (htmlString, submissionId) => dispatch => {
+    const internalApiUrl = getEnvironmentVariable('internalApiUrl');
+
+    const formPath = `/api/v1/HtmlToPdf/${submissionId}`;
+    const fetchOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'text/html',
+            'Accept': 'application/pdf'
+        },
+        body: htmlString
+    };
+    return fetch(`${internalApiUrl}${formPath}`, fetchOptions).then(res => res.blob()).then(blob => {
+        saveFileContent(blob);
+    });
+}
