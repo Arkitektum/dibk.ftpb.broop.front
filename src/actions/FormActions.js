6308f6a6a5ed8d39a17141350e5dc096f7043f2c
Benjamin Dehli <superelg@gmail.com>
2021-09-14 10:52:12 +0200

    Add action for municipality names

--- src/actions/FormActions.js
+++ src/actions/FormActions.js
@@ -4,12 +4,33 @@ import { FETCH_SELECTED_FORM, UPDATE_SELECTED_FORM, SAVE_SELECTED_FORM } from 'c
 // Helpers
 import { getEnvironmentVariable } from 'helpers/environmentVariableHelpers.js';
 
+const getKommunenavnAsync = form => {
+    return new Promise((resolve, reject) => {
+        if (form?.formData?.eiendomByggested?.[0]?.kommunenavn) {
+            resolve(form.formData.eiendomByggested[0].kommunenavn);
+        } else if (form?.formData?.eiendomByggested?.[0]?.eiendomsidentifikasjon?.kommunenummer) {
+            const kommunenummer = form.formData.eiendomByggested[0].eiendomsidentifikasjon.kommunenummer
+            const apiUrl = `https://test-admbygg.dibk.no/api/municipalities/${kommunenummer}`;
+            return fetch(apiUrl).then(res => res.json()).then(municipality => {
+                resolve(municipality.Name);
+            });
+        } else {
+            reject('Finner ikke kommunenavn');
+        }
+    })
+}
+
+
 export const fetchSelectedForm = submission => dispatch => {
     const internalApiUrl = getEnvironmentVariable('internalApiUrl');
     const formType = submission?.innsendingsType?.toLowerCase();
     const formPath = submission._links?.[formType]?.href;
     return fetch(`${internalApiUrl}${formPath}`).then(res => res.json()).then(form => {
-        return dispatch({ type: FETCH_SELECTED_FORM, payload: form })
+        getKommunenavnAsync(form).then(kommunenavn => {
+            form.formData.eiendomByggested[0].kommunenavn = kommunenavn;
+            return dispatch({ type: FETCH_SELECTED_FORM, payload: form })
+        })
+        
     });
 }
 
@@ -18,7 +39,7 @@ export const updateSelectedForm = form => dispatch => {
         dispatch({ type: UPDATE_SELECTED_FORM, payload: form });
         resolve(form);
     })
-    
+
 }
 
 export const saveSelectedForm = form => dispatch => {
