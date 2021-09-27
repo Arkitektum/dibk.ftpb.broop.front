c17a6bde5242cdfe399565074312fd1c854c055e
Benjamin Dehli <superelg@gmail.com>
2021-09-27 13:52:17 +0200

    Adjust template for 'eiendom/byggested'

--- src/components/partials/Forms/FormParts/EiendomByggested.js
+++ src/components/partials/Forms/FormParts/EiendomByggested.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 // DIBK Design
-import { Accordion } from 'dibk-design';
+import { Accordion, Label } from 'dibk-design';
 
 // Helpers
 import { formatAddress } from 'helpers/formatHelpers';
@@ -14,8 +14,25 @@ import formsStyle from 'components/partials/Forms/Forms.module.scss';
 
 class EiendomByggested extends Component {
 
-    renderEiendomByggestedItem(eiendomByggestedItem) {
+    getMunicipalityField(eiendomByggested) {
+        let municipalityField;
+        if (eiendomByggested.kommunenavn) {
+            municipalityField = {
+                label: 'Kommune',
+                value: eiendomByggested.kommunenummer ? `${eiendomByggested.kommunenavn} (${eiendomByggested.kommunenummer})` : `${eiendomByggested.kommunenavn}`
+            }
+        } else if (eiendomByggested.kommunenummer) {
+            municipalityField = {
+                label: 'Kommunenummer',
+                value: eiendomByggested.kommunenummer
+            }
+        }
+        return municipalityField;
+    }
+
+    renderEiendomByggestedItem(eiendomByggested) {
         const isPrint = localStorage.print === "true";
+        const municipalityField = this.getMunicipalityField(eiendomByggested);
         return (
             <div key={0} className="flex">
                 {
@@ -28,30 +45,58 @@ class EiendomByggested extends Component {
                         : ''
                 }
                 <dl className={formsStyle.fieldList}>
+                    {
+                        municipalityField
+                            ? (
+                                <div className="print-flex-10">
+                                    <dt><Label>{municipalityField.label}</Label></dt><dd>{municipalityField.value}</dd>
+                                </div>
+                            )
+                            : ''
+                    }
+
                     <div className="print-flex-10">
-                        <dt>Gårdsnr.</dt><dd>{eiendomByggestedItem.gaardsnummer}</dd>
-                    </div>
-                    <div className="print-flex-10">
-                        <dt>Bruksnr.</dt><dd>{eiendomByggestedItem.bruksnummer}</dd>
-                    </div>
-                    <div className="print-flex-10">
-                        <dt>Festenr.</dt><dd>{eiendomByggestedItem.festenummer}</dd>
-                    </div>
-                    <div className="print-flex-10">
-                        <dt>Seksjonsnr.</dt><dd>{eiendomByggestedItem.seksjonsnummer}</dd>
-                    </div>
-                    <div className="print-flex-10">
-                        <dt>Bygningsnr.</dt><dd>{eiendomByggestedItem.bygningsnummer}</dd>
+                        <dt><Label>Gårdsnr.</Label></dt><dd>{eiendomByggested.gaardsnummer}</dd>
                     </div>
                     <div className="print-flex-10">
-                        <dt>Bolignr.</dt><dd>{eiendomByggestedItem.bolignummer}</dd>
+                        <dt><Label>Bruksnr.</Label></dt><dd>{eiendomByggested.bruksnummer}</dd>
                     </div>
-                    <div className="print-flex-10">
-                        <dt>Kommunenr.</dt><dd>{eiendomByggestedItem.kommunenummer}</dd>
-                    </div>
-                    <div className="print-flex-100">
-                        <dt>Adresse</dt><dd>{formatAddress(eiendomByggestedItem)}</dd>
-                    </div>
+                    {
+                        eiendomByggested.festenummer
+                            ? (
+                                <div className="print-flex-10">
+                                    <dt><Label>Festenr.</Label></dt><dd>{eiendomByggested.festenummer}</dd>
+                                </div>
+                            )
+                            : ''
+                    }
+                    {
+                        eiendomByggested.seksjonsnummer
+                            ? (
+                                <div className="print-flex-10">
+                                    <dt><Label>Seksjonsnr.</Label></dt><dd>{eiendomByggested.seksjonsnummer}</dd>
+                                </div>
+                            )
+                            : ''
+                    }
+                    {
+                        eiendomByggested.bygningsnummer
+                            ? (
+                                <div className="print-flex-10">
+                                    <dt><Label>Bygningsnr.</Label></dt><dd>{eiendomByggested.bygningsnummer}</dd>
+                                </div>
+                            )
+                            : ''
+                    }
+                    {
+                        eiendomByggested.bolignummer
+                            ? (
+                                <div className="print-flex-10">
+                                    <dt><Label>Bolignr.</Label></dt><dd>{eiendomByggested.bolignummer}</dd>
+                                </div>
+                            )
+                            : ''
+                    }
                 </dl>
             </div>
         )
@@ -61,7 +106,8 @@ class EiendomByggested extends Component {
         const adresse = formatAddress(eiendomByggested);
         const gaardsnummer = eiendomByggested?.gaardsnummer;
         const bruksnummer = eiendomByggested?.bruksnummer;
-        const title = `${adresse} - ${gaardsnummer}/${bruksnummer}`;
+        const title = eiendomByggested.adresselinje1?.length ? `${adresse} (${gaardsnummer}/${bruksnummer})` : adresse;
+
         return (
             <div key={index} className={formsStyle.accordionItem}>
                 <Accordion title={title}>
