bec8d471d393757fc13892a93517f0c7fcb42c27
Benjamin Dehli <superelg@gmail.com>
2021-10-15 09:24:40 +0200

    Add sign out button to navigation bar

--- src/components/partials/MainNavigationBar.js
+++ src/components/partials/MainNavigationBar.js
@@ -1,16 +1,61 @@
 // Dependencies
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 // DIBK Design
-import { NavigationBar } from 'dibk-design';
+import { Button, NavigationBar } from 'dibk-design';
 
+// Actions
+import { signOut } from 'actions/IsSignedInActions';
+
+// Stylesheets
+import style from 'components/partials/MainNavigationBar.module.scss';
 
 class MainNavigationBar extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirect: null
+    }
+    this.handleOnLogOut = this.handleOnLogOut.bind(this);
+  }
+
+  componentDidUpdate() {
+    if (this.state.redirect) {
+      this.setState({ redirect: null });
+    }
+  }
+
+  handleOnLogOut() {
+    this.props.signOut();
+    this.setState({
+      redirect: `/skjema/${this.props.selectedForm.referanseId}`
+    })
+  }
+
   render() {
-    return (<NavigationBar logoLink="/" />)
+    if (this.state.redirect) {
+      return <Redirect to={this.state.redirect} />;
+    } else {
+      return (
+        <NavigationBar logoLink="/">
+          <div className={style.buttonContainer}>
+            <Button content="Logg ut" size="small" onClick={() => this.handleOnLogOut()} />
+          </div>
+        </NavigationBar>
+      )
+    }
   }
 }
 
-export default connect(null, null)(MainNavigationBar);
+const mapStateToProps = state => ({
+  selectedForm: state.selectedForm
+});
+
+const mapDispatchToProps = {
+  signOut
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainNavigationBar);
