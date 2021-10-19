ad2ab9a7eb5bb630c32813beab6f55f9d369da90
Benjamin Dehli <superelg@gmail.com>
2021-10-19 15:36:00 +0200

    Remove actions and reducers for temporary dummy authentication

--- src/actions/IsSignedInActions.js
+++
@@ -1,9 +0,0 @@
-import { UPDATE_IS_SIGNED_IN } from 'constants/types';
-
-export const signIn = () => (dispatch) => {
-    dispatch({ type: UPDATE_IS_SIGNED_IN, payload: true });
-}
-
-export const signOut = () => (dispatch) => {
-    dispatch({ type: UPDATE_IS_SIGNED_IN, payload: false });
-}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,7 +9,6 @@ import SelectedSubmissionReducer from 'reducers/SelectedSubmissionReducer';
 import SnackbarMessageReducer from 'reducers/SnackbarMessageReducer';
 import SnackbarLastInitReducer from 'reducers/SnackbarLastInitReducer';
 import SnackbarVisibleReducer from 'reducers/SnackbarVisibleReducer';
-import IsSignedInReducer from 'reducers/IsSignedInReducer';
 
 const reducers = history => combineReducers({
   router: connectRouter(history),
@@ -18,8 +17,7 @@ const reducers = history => combineReducers({
   selectedSubmission: SelectedSubmissionReducer,
   snackbarMessage: SnackbarMessageReducer,
   snackbarLastInit: SnackbarLastInitReducer,
-  snackbarVisible: SnackbarVisibleReducer,
-  isSignedIn: IsSignedInReducer
+  snackbarVisible: SnackbarVisibleReducer
 });
 
 export default reducers;
