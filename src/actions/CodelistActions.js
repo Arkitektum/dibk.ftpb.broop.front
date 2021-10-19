587b793a0b337e8c9b8a1d467c6abe0e4cda33d0
Benjamin Dehli <superelg@gmail.com>
2021-10-19 11:00:03 +0200

    Remove unused actions and reducers for 'funksjon' codelist

--- src/actions/CodelistActions.js
+++
@@ -1,8 +0,0 @@
-import { FETCH_CODELIST_FUNSKJON } from 'constants/types';
-
-export const fetchCodelistFunksjon = (guid) => dispatch => {
-    const apiUrl = 'https://register.dev.geonorge.no/api/kodelister/byggesoknad/funksjon.json?'
-    return fetch(apiUrl).then(res => res.json()).then(codelist => {
-        return dispatch({ type: FETCH_CODELIST_FUNSKJON, payload: codelist })
-    });
-}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,6 @@ import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 
 // Reducers
-import CodelistFunksjonReducer from 'reducers/CodelistFunksjonReducer';
 import SelectedFormReducer from 'reducers/SelectedFormReducer';
 import SelectedSubmissionReducer from 'reducers/SelectedSubmissionReducer';
 import SnackbarMessageReducer from 'reducers/SnackbarMessageReducer';
@@ -13,7 +12,6 @@ import IsSignedInReducer from 'reducers/IsSignedInReducer';
 
 const reducers = history => combineReducers({
   router: connectRouter(history),
-  codelistFunksjon: CodelistFunksjonReducer,
   selectedForm: SelectedFormReducer,
   selectedSubmission: SelectedSubmissionReducer,
   snackbarMessage: SnackbarMessageReducer,
