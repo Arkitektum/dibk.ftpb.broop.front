a44585548c6582ccbe9e5b69fbdb4987324b010b
Benjamin Dehli <superelg@gmail.com>
2021-10-19 09:29:46 +0200

    Add components and userManager util for OIDC

---
+++ src/utils/userManager.js
@@ -0,0 +1,31 @@
+// Dependencies
+import {createUserManager} from 'redux-oidc';
+
+// Helpers
+import {getEnvironmentVariable} from 'helpers/environmentVariableHelpers.js';
+
+const configIsLoaded = () => {
+  return new Promise((resolve, reject) => {
+    if (getEnvironmentVariable('oidc_client_id')) {
+      const userManagerConfig = {
+        client_id: getEnvironmentVariable('oidc_client_id'),
+        authority: getEnvironmentVariable('oidc_authority'),
+        redirect_uri: getEnvironmentVariable('oidc_redirect_uri'),
+        post_logout_redirect_uri: getEnvironmentVariable('oidc_post_logout_redirect_uri'),
+        response_type: "code",
+        scope: "openid profile arkitektum:ansakointernalapi",
+        loadUserInfo: false
+      }
+      resolve(userManagerConfig);
+    } else {
+      window.setTimeout(() => {
+        resolve(configIsLoaded());
+      }, 100)
+    }
+  });
+}
+const getUserManagerConfigWhenReady = configIsLoaded().then((userManagerConfig) => {
+  return createUserManager(userManagerConfig);
+})
+
+export default getUserManagerConfigWhenReady;
